6ec1aedd1cfc9e8c1f5ad5c14da608402f95b320
JorianG <88860300+JorianG@users.noreply.github.com>
2022-03-19 16:38:55 +0100

    add class Salle delete calss Droite rework Individu TODO finish maths for droite

--- classIndividu.py
+++ classIndividu.py
@@ -1,29 +1,63 @@
+# coding: utf-8
 
 class Individu:
+
     def __init__(self, x, y):
         self.position= (x, y)
-        self.chemin = None 
-class Salle:
-    def __init__(self,longueur, largeur):
-        self.longueur = longueur
-        self.largeur = largeur
-class Droites:
-    def __init__(self):
         self.coefDirect = None
         self.ordOrigine = None
-        
-    def droiteCheminNaif(Individu, arrivee):
-        #arrivee est un tuple avec les coords
-        if Individu.position[0]== arrivee[0]:
-            print("lol")
-            #droite horizontale
-        else:
-            self.coefDirect = (Individu.position[1] - arrivee[1]) / (Individu.position[0] - arrivee[0])
-            self.ordOrigine = Individu.position[1]-(coefDirect*Individu.position[0])
-            if Individu.position[0] > arrivee[0]:
-                self.sens= -1
+        self.sens= None
+        Salle.individu.append(self)
+    
+    
+    def droiteCheminNaif(self, Individu, Salle.arrivee):
+        #Salle.arrivee est un tuple avec les coords
+        if Individu.position[0] == Salle.arrivee[0]:
+            if Individu.position[1] == Salle.arrivee[1]:
+                Individu.isDrawn == False
             else:
-                self.sens= 1
+                #droite horizontale
+                #TODO
+                pass
+
+        else:
+            self.coefDirect = (Individu.position[1] - Salle.arrivee[1]) / (Individu.position[0] - Salle.arrivee[0])
+            self.ordOrigine = Individu.position[1]-(self.coefDirect*Individu.position[0])
+            if Individu.position[0] > Salle.arrivee[0]:
+                self.sens = -1
+            else:
+                self.sens = 1
+    
+    def supprimer_balle(self,Salle):
+        '''
+        Supprime la balle de la liste des balles en jeu
+        In: jeu
+        Out: None
+        '''
+        if len(Salle.individu) != 0:    
+            for i in range (len(Salle.individu)):
+                if Salle.individu[i]==self:
+                    Salle.individu.pop(i)
+                    break
+            
+
+    def deplacement(self, vitesse):
+        x = self.position[0] + self.sens * vitesse
+        y = self.coefDirect * x + self.ordOrigine
+        self.position = (x,y)
+
+    def deplacementVertical(self, vitesse):
+        y = self.position[1] + self.sens * vitesse
+        self.position = (self.position[0], y)
+
+
+class Salle:
+    def __init__(self):
+        self.x = 800
+        self.y = 600
+        self.individu = []
+        self.arrivee = (0,300)
+    
 class Obstacles:
     def __init__(self, x, y):
         self.position = (x,y)
